602a7c224f2197a233caa50f2300c9521ab466b9
Murkhunting <marcmp.arq@gmail.com>
2025-05-04 00:56:53 +0200

    static pages

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  safelist: ["glass"], // <-- Esto asegura que no se elimine tu clase
+  plugins: [],
+};
